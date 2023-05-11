81776dd08b9a8957efabf7f4d2bbb6473c5d8236
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-11 11:39:47 +0200

    Create a class MyList that has an instance variable @list.
    #add method #each that yields successive members of @list and uses the MyEnumerable module to Mylist
    
    Create a module MyEnumerable that implements the following methods (they should have the same funcionality as methods in Enumerable):

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,24 +1,23 @@
 module MyEnumerable
-    def all?
-      @list.each do |i|
-        return false unless yield i
-      end
-      true
+  def all?
+    @list.each do |i|
+      return false unless yield i
     end
-  
-    def any?
-      @list.each do |i|
-        return true if yield i
-      end
-      false
-    end
-  
-    def filter
-      result = []
-      @list.each do |i|
-        result << i if yield i
-      end
-      result
-    end
+    true
   end
-  
+
+  def any?
+    @list.each do |i|
+      return true if yield i
+    end
+    false
+  end
+
+  def filter
+    result = []
+    @list.each do |i|
+      result << i if yield i
+    end
+    result
+  end
+end
